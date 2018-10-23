b8eadd4a2a7f7d2caeb4f2594f2b63e219ce5119
Niklas Andersson <niklas.andersson@student.kyh.se>
2018-10-23 15:06:29 +0200

    La teee lite i hello

--- test.py
+++ test.py
@@ -4,4 +4,12 @@ def mul_func(a, b):
 def add_func(a, b):
     return a + b
 
-print("Hello world")
+def hello_world():
+    print("Hello world")
+def main():
+    result = add(2, 3)
+    print(result)
+    hello_world()
+
+if __name__ == '__main__':
+    main()
